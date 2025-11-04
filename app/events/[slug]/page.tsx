2c8ecb31df816d57ff84666a702bd1635ac6384d
Tanish196 <tanishjagetiya@gmail.com>
2025-11-04 18:53:54 +0530

    Suspense added

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,13 +1,13 @@
-import {Suspense} from "react";
-import EventDetails from "@/components/EventDetails";
+import { Suspense } from "react"
+import EventDetails from "@/components/EventDetails"
+const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }> }) => {
 
-const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }>}) => {
-    const slug = params.then((p) => p.slug);
+    const slug = params.then((p)=>p.slug);
 
     return (
         <main>
-            <Suspense fallback={<div>Loading...</div>}>
-                <EventDetails params={slug} />
+            <Suspense fallback={<div>Loading....</div>}>
+                <EventDetails params={slug}/>
             </Suspense>
         </main>
     )
