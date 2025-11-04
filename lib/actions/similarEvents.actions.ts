78a5ed4c24b81cb491ace12d111874f982bc8dec
Tanish196 <tanishjagetiya@gmail.com>
2025-11-04 18:20:28 +0530

    Deployment fix 3

--- lib/actions/similarEvents.actions.ts
+++ lib/actions/similarEvents.actions.ts
@@ -2,17 +2,24 @@
 
 import { Event } from "@/database"
 import connectToDatabase from "../mongodb"
+import { unstable_cache } from 'next/cache'
 
-export const findSimilarEvents = async (slug: string) => {
-    try {
-        await connectToDatabase()
-        const event = await Event.findOne({ slug })
-        if (!event) {
+export const findSimilarEvents = unstable_cache(
+    async (slug: string) => {
+        try {
+            await connectToDatabase()
+            const event = await Event.findOne({ slug })
+            if (!event) {
+                return []
+            }
+            return await Event.find({ _id: { $ne: event._id }, tags: { $in: event.tags } }).lean()
+        } catch {
             return []
         }
-        // const similarEvents = 
-        return await Event.find({ _id: { $ne: event._id }, tags: { $in: event.tags } }).lean()
-    } catch {
-        return []
+    },
+    ['similar-events'],
+    {
+        revalidate: 3600, // Cache for 1 hour
+        tags: ['similar-events']
     }
-}
+)
