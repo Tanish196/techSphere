69e26aeb283b0a891b2bcb9f2ee846cf41ce51e4
Tanish196 <tanishjagetiya@gmail.com>
2026-02-18 17:43:46 +0530

    Landing Page fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
 
                 <ul>
                     <Link href="/">Home</Link>
-                    <Link href="/">Events</Link>
+                    <Link href="/events">Events</Link>
                     <Link href="/events/create">Create Event</Link>
                 </ul>
             </nav>
